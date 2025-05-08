6745d0ca65c0d288ec84885f572130cd358bc851
utopiaswap <swaputopia@proton.me>
2025-05-08 21:12:53 +0100

    changed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://utopiaswap.github.io",
+  url: "https://docs.utopiaswap.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/utopiaswapDOCS/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
